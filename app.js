2737e1eaeb82c0e3e69674a922f110855e1e88f5
Saikiran Dontula <ksai0509@gmail.com>
2019-02-20 09:34:23 +0000

    reg

--- app.js
+++ app.js
@@ -375,7 +375,7 @@ app.post("/register",upload.single('avatar'),function(req,res){
   // req.connection.remoteAddress will provide IP address of connected user.
   var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
   // Hitting GET request to the URL, Google will respond with success or error scenario.
-  request(verificationUrl,function(error,response,body) {
+  req(verificationUrl,function(error,response,body) {
     body = JSON.parse(body);
     // Success will be true or false depending upon captcha validation.
     if(body.success !== undefined && !body.success) {
@@ -383,7 +383,7 @@ app.post("/register",upload.single('avatar'),function(req,res){
     }
     res.json({"responseCode" : 0,"responseDesc" : "Sucess"});
   });
-            req.flash("success","Welcome");
+            req.flash("success","Welcome " + user.username);
             res.redirect("/blogs/recent");
         });
     });
